1f5cc4e2c3f348663a768053bd12783d206834f2
Chun Yi <8.5by11@gmail.com>
2022-01-14 21:39:17 -0500

    commit tree 5d5d030a6a664c7ee8da877a584baf341982d1e5

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
-// Code your solution here
+function findMatching(drivers, name) {
+    return drivers.filter(names => names.toLowerCase() === name.toLowerCase());
+}
+
+findMatching(drivers, name);
+
+function fuzzyMatch(drivers, string) {
+    return drivers.filter(names => names.slice(0, 2) === string);
+}
+
+fuzzyMatch(drivers);
+
+function matchName(drivers, name) {
+    return drivers.filter(driver => driver.name === name) 
+}
+
+matchName(drivers);
